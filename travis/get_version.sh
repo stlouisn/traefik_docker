01f2965687ef1bdc20248d17b8194ccda6f6e1e4
stlouisn <stlouisn@hotmail.com>
2020-01-30 19:48:25 -0400

    Update get_version.sh

--- travis/get_version.sh
+++ travis/get_version.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
-# Output traefik version from github:traefik releases
-#echo "$(curl -fsSL --retry 5 --retry-delay 2 https://github.com/containous/traefik/releases/ | grep -o '[0-9]\?[0-9].[0-9]\?[0-9].[0-9]\?[0-9]/traefik_linux-amd64' | awk -F '/' {'print $1'} | head -n 1)"
-echo "$(curl -ksSL --retry 5 --retry-delay 2 https://github.com/containous/traefik/releases/latest | grep -o '[0-9]\?[0-9].[0-9]\?[0-9].[0-9]\?[0-9]/traefik_v2' | awk -F '/' {'print $1'} | head -n 1)"
+# Output traefik [[ V1 ]] version from github:traefik releases
+echo "$(curl -fsSL --retry 5 --retry-delay 2 https://github.com/containous/traefik/releases/ | grep -o '[0-9]\?[0-9].[0-9]\?[0-9].[0-9]\?[0-9]/traefik_linux-amd64' | awk -F '/' {'print $1'} | head -n 1)"
+
+# Output traefik [[ V2 ]] version from github:traefik releases
+#echo "$(curl -ksSL --retry 5 --retry-delay 2 https://github.com/containous/traefik/releases/latest | grep -o '[0-9]\?[0-9].[0-9]\?[0-9].[0-9]\?[0-9]/traefik_v2' | awk -F '/' {'print $1'} | head -n 1)"
