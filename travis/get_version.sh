2f2f3fbd619a333e5dab00d44eba56bfb6033448
stlouisn <stlouisn@hotmail.com>
2020-01-30 13:53:32 -0400

    Update get_version.sh

--- travis/get_version.sh
+++ travis/get_version.sh
@@ -3,4 +3,5 @@
 set -euo pipefail
 
 # Output traefik version from github:traefik releases
-echo "$(curl -fsSL --retry 5 --retry-delay 2 https://github.com/containous/traefik/releases/ | grep -o '[0-9]\?[0-9].[0-9]\?[0-9].[0-9]\?[0-9]/traefik_linux-amd64' | awk -F '/' {'print $1'} | head -n 1)"
+#echo "$(curl -fsSL --retry 5 --retry-delay 2 https://github.com/containous/traefik/releases/ | grep -o '[0-9]\?[0-9].[0-9]\?[0-9].[0-9]\?[0-9]/traefik_linux-amd64' | awk -F '/' {'print $1'} | head -n 1)"
+echo "$(curl -ksSL --retry 5 --retry-delay 2 https://github.com/containous/traefik/releases/latest | grep -o '[0-9]\?[0-9].[0-9]\?[0-9].[0-9]\?[0-9]/traefik_v2' | awk -F '/' {'print $1'} | head -n 1)"
