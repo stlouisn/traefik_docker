cc2c52f933d524a2b658c71eaefd93e2cf9f4ceb
stlouisn <stlouisn@hotmail.com>
2020-01-31 07:00:00 -0400

    Update get_version.sh

--- travis/get_version.sh
+++ travis/get_version.sh
@@ -2,8 +2,9 @@
 
 set -euo pipefail
 
-# Output traefik [[ V1 ]] version from github:traefik releases
-echo "$(curl -fsSL --retry 5 --retry-delay 2 https://github.com/containous/traefik/releases/ | grep -o '[0-9]\?[0-9].[0-9]\?[0-9].[0-9]\?[0-9]/traefik_linux-amd64' | awk -F '/' {'print $1'} | head -n 1)"
-
-# Output traefik [[ V2 ]] version from github:traefik releases
-#echo "$(curl -ksSL --retry 5 --retry-delay 2 https://github.com/containous/traefik/releases/latest | grep -o '[0-9]\?[0-9].[0-9]\?[0-9].[0-9]\?[0-9]/traefik_v2' | awk -F '/' {'print $1'} | head -n 1)"
+# Output traefik version from github:traefik releases
+if [ $DOCKER_TAG == "1" ]; then
+  echo "$(curl -fsSL --retry 5 --retry-delay 2 https://github.com/containous/traefik/releases/ | grep -o '[0-9]\?[0-9].[0-9]\?[0-9].[0-9]\?[0-9]/traefik_linux-amd64' | awk -F '/' {'print $1'} | head -n 1)"
+elif [ $DOCKER_TAG == "2" ]; then
+  echo "$(curl -ksSL --retry 5 --retry-delay 2 https://github.com/containous/traefik/releases/latest | grep -o '[0-9]\?[0-9].[0-9]\?[0-9].[0-9]\?[0-9]/traefik_v2' | awk -F '/' {'print $1'} | head -n 1)"
+fi
